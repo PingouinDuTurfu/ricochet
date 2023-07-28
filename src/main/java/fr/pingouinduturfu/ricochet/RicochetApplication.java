b69192d6ec210e5840acbcb0d739a17adb1baf88
Rémy Ollivier <remy.ollivier64@gmail.com>
2023-07-28 09:26:13 +0100

    Initialisation

---
+++ src/main/java/fr/pingouinduturfu/ricochet/RicochetApplication.java
@@ -0,0 +1,13 @@
+package fr.pingouinduturfu.ricochet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicochetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RicochetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/pingouinduturfu/ricochet/RicochetApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.pingouinduturfu.ricochet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicochetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
