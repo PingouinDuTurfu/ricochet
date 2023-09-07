2b536fd4a0ad22676207845577945fdeda75d1a5
Rémy Ollivier <remy.ollivier64@gmail.com>
2023-09-07 16:36:52 +0200

    Add websocket connection for games

--- src/main/java/fr/pingouinduturfu/ricochet/controller/AppController.java
+++ src/main/java/fr/pingouinduturfu/ricochet/controller/AppController.java
@@ -1,9 +1,9 @@
 package fr.pingouinduturfu.ricochet.controller;
 
-import fr.pingouinduturfu.ricochet.persistence.MapPersistence;
-import fr.pingouinduturfu.ricochet.repository.MapRepository;
+import fr.pingouinduturfu.ricochet.exception.MapManagerException;
+import fr.pingouinduturfu.ricochet.game.manager.MapManager;
+import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,12 +12,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+import java.util.Map;
+
 
 @Controller
+@RequiredArgsConstructor
 public class AppController {
 
-    @Autowired
-    private MapRepository mapRepository;
+    private final MapManager mapManager;
 
     @GetMapping("")
     public String showRoot() {
@@ -31,26 +34,28 @@ public class AppController {
 
     @PostMapping(value = "/getMapsNames", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<JSONObject> getMapsNames() {
-        JSONObject json = new JSONObject();
-        json.put("mapsNames", mapRepository.findAllNames());
-        System.out.println("mapsNames: " + json.get("mapsNames"));
-        return ResponseEntity.status(HttpStatus.OK).body(json);
+        Map<String, List<String>> jsonMap = Map.of("mapsNames", mapManager.getMapsNames());
+        return ResponseEntity.status(HttpStatus.OK).body(new JSONObject(jsonMap));
     }
 
     @PostMapping(value = "/saveMap", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveMap(@Param("name") String name, @Param("mapData") String mapData) {
-        System.out.println("name: " + name);
-        System.out.println("data: " + mapData);
-        mapRepository.save(new MapPersistence(name, mapData));
+        try {
+            mapManager.saveMap(name, mapData);
+        } catch (MapManagerException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PostMapping(value = "/loadMap", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<JSONObject> loadMap(@Param("name") String name) {
-        MapPersistence map = mapRepository.findFirstByName(name);
-        JSONObject json = new JSONObject();
-        json.put("mapData", map.getData());
-        return ResponseEntity.status(HttpStatus.OK).body(json);
+        try {
+            Map<String, String> jsonMap = Map.of("mapData", mapManager.loadMap(name));
+            return ResponseEntity.status(HttpStatus.OK).body(new JSONObject(jsonMap));
+        } catch (MapManagerException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 
 }
